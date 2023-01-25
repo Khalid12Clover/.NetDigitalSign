1c3d44f26750758d3447fbba5a5107f3f81a574e
Khalid12Clover <92978759+Khalid12Clover@users.noreply.github.com>
2023-01-25 17:21:48 +0530

    Update server_start.sh

--- deployscripts/server_start.sh
+++ deployscripts/server_start.sh
@@ -1,5 +1,2 @@
-#!/usr/bin/env bash
-cd /var/www
-dotnet publish -c Release -r linux-x64 --self-contained=true -p:PublishSingleFile=true -p:GenerateRuntimeConfigurationFiles=true -o artifacts
-sudo systemctl stop dnsserver.service
-sudo systemctl start dnsserver.service
+sudo systemctl stop webapi.service
+sudo systemctl start webapi.service
