9b9dc8b09b68bc45507464c9dfbfe4dea9176126
Khalid12Clover <92978759+Khalid12Clover@users.noreply.github.com>
2023-01-25 16:53:45 +0530

    Update server_start.sh

--- deployscripts/server_start.sh
+++ deployscripts/server_start.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 cd /var/www
 dotnet publish -c Release -r linux-x64 --self-contained=true -p:PublishSingleFile=true -p:GenerateRuntimeConfigurationFiles=true -o artifacts
-sudo systemctl start dnsserver.service
 sudo systemctl stop dnsserver.service
+sudo systemctl start dnsserver.service
